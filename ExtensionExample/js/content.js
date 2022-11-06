a0e5c87782ce855449aedbd5c5f9b65574b36491
RAYT21 <90043295+RAYT21@users.noreply.github.com>
2022-11-06 21:43:03 +0300

    Приколы прикольные

--- ExtensionExample/js/content.js
+++ ExtensionExample/js/content.js
@@ -1,50 +1,35 @@
-// Когда приходит сообщение из popup.js
-chrome.runtime.onMessage.addListener(
-    function (data, sender, sendResponse) {
-        //сообщение из popup.js
-        parseMessage(data)
 
-        // Если нужно отправить ответ в popup.js
-        sendResponse({"data": "Content принял"});
+//сохраниение исходной страницы
+(function() {
+    let page = document.documentElement.textContent;
+    console.log(page);
+    if (localStorage) { // Browser supports it
+        localStorage.openPage = page;
     }
-);
+})();
 
-// Обрабатываем входящии сообщение
-function parseMessage(data) {
-    console.log(data);
-    if (data.data.type) {
-        var messageData = data.data;
-        console.log(messageData.type);
-        // Меняем фон
-        if (messageData.type == 'color') {
-            var color = messageData.color;
-            document.body.style.background = color;
-        }
+// динамическое добавление элементов в DOM
+function nodeInsertedCallback(event) {
+    console.log(event.target.textContent);
+    if (localStorage) { // Browser supports it
+        localStorage.newPage = event;
     }
 }
 
-// Отправить сообщение в popup.js
-function sendPopup(data) {
-    chrome.runtime.sendMessage({"data": data}, function (response) {
-        console.log(response);
-    });
+function cutTags(str) {
+    //let regex = /( |<([^>]+)>)/ig;
+    let regex = /[^А-Яа-я]/ig;
+    return str.replace(regex, "");
 }
 
-
-// При клике на любой div передаем его html в popup
-var ElemArr = document.querySelectorAll('div');
-if (ElemArr.length) {
-    ElemArr.forEach(function (Elem) {
-        Elem.onclick = function onClick(event) {
-            //event.preventDefault();
-            $this = event.target;
-
-
-            sendPopup({"type": 'html', 'html': $this.innerHTML});
-
-            //chrome.runtime.sendMessage({color: color});
-
-            //return false;
-        };
-    });
+function cutScripts(str){
+    let regex = /<script([^`])*<\/script>/ig;
+    return str.replace(regex, "");
 }
+
+function cutCSS(str){
+    let regex = /(.|#)?([^{}]+)\{([^}]+)}/ig;
+    return str.replace(regex, "");
+}
+
+document.addEventListener('DOMNodeInserted', nodeInsertedCallback);
